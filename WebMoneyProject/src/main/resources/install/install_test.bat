954ab583567c29a9bc21c120d1cc4d5d99b4460e
Dale <Dale@Dale-PC>
2016-02-28 21:08:44 +0600

    Финал. Без юнитов

--- WebMoneyProject/src/main/resources/install/install_test.bat
+++ WebMoneyProject/src/main/resources/install/install_test.bat
@@ -1,4 +1,4 @@
-SET MYSQLROOT=root
-SET PASSWD=krutoy
-mysql -h127.0.0.1 -u%MYSQLROOT% -p%PASSWD% < install_test.sql
+SET MYSQLROOT=postgres
+SET PASSWD=Ny042012
+psql  -hlocalhost -U%MYSQLROOT% -finstall_test.sql -a -oinstall_test.res -Linstall_test.log
 PAUSE
