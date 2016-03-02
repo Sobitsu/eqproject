8e8cd6296bd8602270c961debb078f8ad5d6f6ba
Dale <Dale@Dale-PC>
2016-03-02 14:02:05 +0600

    Финал. Без юнитов

---
+++ WebMoneyProject/src/main/java/org/hc/wm/actives/stock/Stock.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hc.wm.actives.stock;
+
+import org.hc.wm.actives.Actives;
+
+/**
+ *
+ * @author Dale
+ */
+public class Stock implements Actives {
+    
+}
